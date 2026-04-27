12f37ac700e1ad4562c6ff4a9aab968110eba74f
singhakash123 <akashlife2004@gmail.com>
2026-04-27 09:52:29 +0530

    db setup

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
 import { db_connect } from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-db_connect();
+const port = process.env.PORT || 3000;
+db_connect()
+  .then((result) => {
+    app.listen(port, () => {
+      console.log(`Server is listening at : ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error(`Database connection failed : ${err.message}`);
+  });
