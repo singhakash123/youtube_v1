742ba794fb263d5832c0c879d88eae511732cd97
singhakash123 <akashlife2004@gmail.com>
2026-04-28 11:52:04 +0530

    implement user registration with avatar and cover image upload

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,11 +2,56 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../services/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { userName, email, password, fullName } = req.body;
+  const { userName, email, fullName, password } = req.body;
 
-  console.log("username", userName);
+  if (
+    [userName, email, fullName, password].some(
+      (item) => !item || item.trim() === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const exitUser = await User.findOne({
+    $or: [{ email }, { userName }],
+  });
+
+  if (exitUser) {
+    throw new ApiError(409, "User already exists");
+  }
+
+  const avatarLocal = req.files?.avatar?.[0]?.path;
+
+  if (!avatarLocal) {
+    throw new ApiError(400, "Avatar is required");
+  }
+
+  const coverImage = req.files?.coverImage?.[0]?.path;
+
+  const avatarUrl = await uploadOnCloudinary(avatarLocal);
+
+  if (!avatarUrl?.url) {
+    throw new ApiError(400, "Avatar upload failed");
+  }
+
+  let coverImageUrl;
+
+  if (coverImage) {
+    const uploaded = await uploadOnCloudinary(coverImage);
+    coverImageUrl = uploaded?.url;
+  }
+
+  const user = await User.create({
+    userName,
+    email,
+    fullName,
+    password,
+    avatar: avatarUrl.url,
+    coverImage: coverImageUrl || undefined,
+  });
 });
 
 export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,20 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-
+import { upload } from "../middlewares/multer.js";
 const userRouter = Router();
 
-userRouter.route("/register").post(registerUser); // POST    /api/v1/user/register
+userRouter.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverimage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+); // POST    /api/v1/user/register
 
 export { userRouter };
