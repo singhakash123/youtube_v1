b76fa16e7f5f639a06226bf570b4b4b286ae06f3
singhakash123 <akashlife2004@gmail.com>
2026-05-10 10:40:41 +0530

    feat: implement logging functionality using pino and update user controller

---
+++ redme.md
@@ -0,0 +1,3 @@
+console.log → logger.info
+console.error → logger.error
+critical crash → logger.fatal
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,9 @@
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { logger } from "../utils/logger.js";
+import { User } from "../models/user.model.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  
+});
